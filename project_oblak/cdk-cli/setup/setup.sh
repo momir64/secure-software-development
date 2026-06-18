61c39bc01c2a96eee4c593f00316cc1712802928
Momir Stanišić <momir.stanisic.ns@gmail.com>
2026-06-18 23:11:55 +0200

    [FIX] - cli setup script for both bashrc and zsh

--- project_oblak/cdk-cli/setup/setup.sh
+++ project_oblak/cdk-cli/setup/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 DIR="$(cd "$(dirname "$0")/../linux" && pwd)"
-echo "export PATH=\"$DIR:\$PATH\"" >> ~/.bashrc
-echo "Added $DIR to PATH in ~/.bashrc"
-echo "Run 'source ~/.bashrc' or restart your terminal to apply"
+echo "export PATH=\"$DIR:\$PATH\"" >> ~/.profile
+echo "Added $DIR to PATH in ~/.profile"
+echo "Run 'source ~/.profile' or restart your terminal to apply"
